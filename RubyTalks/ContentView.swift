b19bd9d170679761dbdd79f419fcb46edf3b5ee4
KrassCodes <k.iankov@gmail.com>
2020-04-04 09:19:30 +0100

    Initial Commit

---
+++ RubyTalks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RubyTalks
+//
+//  Created by Krassimir Iankov on 4/4/20.
+//  Copyright © 2020 Krassimir Iankov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
