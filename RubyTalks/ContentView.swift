2e855c2254f6fcc4c3dfb8770e2fbbf948393942
KrassCodes <k.iankov@gmail.com>
2020-04-05 10:38:35 +0100

    Connect to API and display talks, bug fixes, formatting fixes

--- RubyTalks/ContentView.swift
+++ RubyTalks/ContentView.swift
@@ -8,9 +8,10 @@
 
 import SwiftUI
 import Combine
+import WebKit
 
 struct Talk: Decodable {
-    let title, description: String
+    let conference_name, speaker_name, speaker_bio, speaker_twitter, talk_title, talk_description, talk_video: String
 }
 
 
@@ -30,13 +31,13 @@ class NetworkManager: ObservableObject {
             guard let data = data else { return }
             
             let talks = try! JSONDecoder().decode([Talk].self, from: data)
-           
+            
             DispatchQueue.main.async {
                 self.talks = talks
             }
             
-            print("completed fetching JSON")
-            print(talks)
+//            print("completed fetching JSON")
+//            print(talks)
             
         }.resume()
     }
@@ -48,13 +49,53 @@ struct ContentView: View {
     @ObservedObject var networkManager = NetworkManager()
     
     var body: some View {
-        NavigationView {
-            List (networkManager.talks, id: \.title) {
-                Text($0.title)
+        NavigationView{
+            List {
+                ForEach(networkManager.talks, id: \.talk_title) { talk in
+                    HStack {
+                        NavigationLink (destination: WebView(request: URLRequest(url: ((URL(string: talk.talk_video) ?? URL(string: "https://apple.com" ))!)))){
+                            Image("cat")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width:150)
+                                .cornerRadius(10)
+                            VStack(alignment: .leading){
+                                Text(talk.conference_name)
+                                    .font(.body)
+                                    .foregroundColor(.secondary)
+                                Text(talk.talk_title)
+                                    .font(.headline)
+                                    .fontWeight(.black)
+                                    .foregroundColor(.primary)
+                                    .lineLimit(3)
+                                Text(talk.speaker_name)
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
                 }
             }.navigationBarTitle(Text("Ruby Talks"))
         }
     }
+}
+
+
+struct WebView : UIViewRepresentable {
+    
+    let request : URLRequest
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(request)
+    }
+    
+}
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
